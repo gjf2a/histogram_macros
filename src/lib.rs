11cf85816c2e2a95b303d199da1a6ff66f80d6db
gjf2a <ferrer@hendrix.edu>
2022-09-17 07:31:19 -0500

    Added weight! and weight_ref! for float counts.

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,27 @@ macro_rules! count {
 #[macro_export]
 macro_rules! count_ref {
     ($d:expr, $k:expr) => {
-        *($d.get($k).unwrap_or(&0))
+        get_skeleton!($d, $k, 0)
+    }
+}
+
+#[macro_export]
+macro_rules! weight {
+    ($d:expr, $k:expr) => {
+        weight_ref!($d, &$k)
+    }
+}
+
+#[macro_export]
+macro_rules! weight_ref {
+    ($d:expr, $k:expr) => {
+        get_skeleton!($d, $k, 0.0)
+    }
+}
+
+macro_rules! get_skeleton {
+    ($d:expr, $k:expr, $z:expr) => {
+        *($d.get($k).unwrap_or(&$z))
     }
 }
 
@@ -63,7 +83,22 @@ mod tests {
         bump_ref!(hist, "walk");
         bump_ref!(hist, "balk");
         assert_eq!(count_ref!(hist, "walk"), 2);
-        println!("{:?}", hist);
+        assert_eq!(count_ref!(hist, "balk"), 1);
+        assert_eq!(count_ref!(hist, "talk"), 1);
+        assert_eq!(count_ref!(hist, "sulk"), 0);
+    }
+
+    #[test]
+    fn test_string() {
+        let mut hist = HashMap::new();
+        bump!(hist, "walk".to_owned());
+        bump!(hist, "talk".to_owned());
+        bump!(hist, "walk".to_owned());
+        bump!(hist, "balk".to_owned());
+        assert_eq!(count!(hist, "walk".to_owned()), 2);
+        assert_eq!(count!(hist, "balk".to_owned()), 1);
+        assert_eq!(count!(hist, "talk".to_owned()), 1);
+        assert_eq!(count!(hist, "sulk".to_owned()), 0);
     }
 
     #[test]
@@ -73,6 +108,17 @@ mod tests {
         bump!(hist, 5);
         bump!(hist, 3);
         assert_eq!(count!(hist, 3), 2);
-        println!("{:?}", hist);
+        assert_eq!(count!(hist, 4), 0);
+        assert_eq!(count!(hist, 5), 1);
+    }
+
+    #[test]
+    fn test_float() {
+        let mut hist = HashMap::new();
+        bump_ref_by!(hist, "hi", 1.5);
+        bump_ref_by!(hist, "bye", 2.6);
+        bump_ref_by!(hist, "hi", 0.3);
+        assert_eq!(weight_ref!(hist, "hi"), 1.8);
+        assert_eq!(weight_ref!(hist, "bye"), 2.6);
     }
 }
